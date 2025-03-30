5fbaacf7bc6fbe50b48913dcea4bcaff75b2068d
Chaima Mansouri <157808654+ChaimaMansouri@users.noreply.github.com>
2025-03-30 22:31:31 +0100

    first commit

---
+++ Front_end/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Men from "./pages/Men";
+import Women from "./pages/Women";
+import Predictions from "./pages/Predictions";
+import General from "./pages/General";
+import PrivateRoute from "./components/privateRoutes";
+
+function App() {
+  return (  
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route element={<PrivateRoute />}>
+        <Route path="/men" element={< Men />} />
+        <Route path="/women" element={< Women />} />
+        <Route path="/predictions" element={<Predictions />} />
+        <Route path="/general" element={<General />} />
+        </Route>
+      </Routes>
+    </Router>
+
+  );
+}
+
+export default App;
